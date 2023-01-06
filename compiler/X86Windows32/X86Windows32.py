1887b9b2e38633de27bf4c3d7b4a8958d1d0556e
Giacomo Casoni <giacomo.casoni@ibm.com>
2023-01-06 17:03:40 +0100

    Added strings

--- compiler/X86Windows32/X86Windows32.py
+++ compiler/X86Windows32/X86Windows32.py
@@ -24,17 +24,19 @@ def to_hex(s):
 class DeclarationStatementAssemblyBuilder(X86Windows32AssemblyBuilder):
     def generate_assembly(self, num_of_variables, init_value=None):
         current_var = (num_of_variables + 1) * 4
+        assembly = []
         if init_value and isinstance(init_value, int):
             init_value = hex(int)
+            assembly.append("       mov [ebp-{}], {};".format(hex(current_var), hex(init_value)))
         elif init_value and isinstance(init_value, str):
-            tmp = init_value[::-1]
-            for x in range(0, len(tmp), 4):
-                
-        assembly = ""
-        if init_value:
-            assembly = [
-                "       mov [ebp-{}], {};".format(hex(current_var), hex(init_value))
-            ]
+            init_value = init_value[1:-1]
+            tmp = bytes(init_value, "ascii")
+            tmp = tmp + b"\x00" * (4 - (len(tmp) % 4))
+            tmp = tmp[::-1]
+            for offset in range(0, len(tmp), 4):
+                sub_string = "0x" + "".join([hex(x).replace("0x", "") for x in tmp[offset:offset+4]])
+                assembly.append("       push {}".format(sub_string))
+            assembly.append("       mov [ebp-{}], esp;".format(hex(current_var)))
         return assembly
 
 class AssignmentStatementAssemblyBuilder(X86Windows32AssemblyBuilder):
@@ -48,9 +50,13 @@ class AssignmentStatementAssemblyBuilder(X86Windows32AssemblyBuilder):
 
 class AdditionStatementAssemblyBuilder(X86Windows32AssemblyBuilder, BinaryOperator):
     def generate_assembly(self, left_hand, right_hand):
-        assembly = [
-            "       add {}, {};".format(left_hand, right_hand)
-        ]
+        assembly = []
+        if not type(left_hand) == type(right_hand):
+            exit("Cannot add operand {} and {}".format(right_hand, left_hand))
+        if isinstance(left_hand, int):
+            assembly.append("       add {}, {};".format(left_hand, right_hand))
+        elif isinstance(left_hand, str):
+            exit("String concatenation not implemented yet")
         return assembly
 
 class SubtractionStatementAssemblyBuilder(X86Windows32AssemblyBuilder, BinaryOperator):
